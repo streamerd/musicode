99aa54f3c68f316a0a5f1d2d241c02b796986102
Abbas Tolgay Yılmaz <abbas.tolgay.yilmaz@protonmail.com>
2021-08-17 16:12:50 +0200

    add rainy day pianos

---
+++ rainy_day_pianos.rb
@@ -0,0 +1,24 @@
+##| rainy day pianos
+
+pianos = "/Users/streamerd/devel/musicode/GrandPiano"
+
+
+live_loop :pia do
+  sample pianos, "piano-f-a1", amp: 2
+  sleep 1
+  sample pianos, "piano-f-a3", amp: 2, attack: 0.5
+  sleep 2
+  
+end
+
+live_loop :no do
+  sleep 4
+  sample pianos, "piano-f-c6", amp: 0.8
+end
+
+live_loop :now do
+  sleep 6
+  sample pianos, "piano-f-c7", amp: 1
+end
+
+
